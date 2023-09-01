0b599c55d944a94dfe14e4b4f196acb5807a5a1d
Nicholas Gerade <nicholasgerade@gmail.com>
2023-08-31 22:50:19 -0300

    Menu Fixed

---
+++ JS/header.js
@@ -0,0 +1,24 @@
+// declarando variáveis
+let ul = document.querySelector('nav ul');
+let menu = document.querySelector('.menu-btn i');
+
+// ao clicar no menu hamburguer abre ele
+function menuShow() {
+    if (ul.classList.contains('open')){
+        ul.classList.remove('open');
+    }
+
+    else{
+        ul.classList.add('open');
+    }
+}
+
+
+
+// declarando variavel
+let ul2 = document.querySelector('nav ul');
+
+// ao scrollar o menu sai
+window.onscroll = () => {
+    ul.classList.remove('open');
+};
